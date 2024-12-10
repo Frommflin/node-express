89dd0e326bb57a0d533d25cf86ab333f3fa62c58
Klara Frommelin <freakyfrommelin@outlook.com>
2024-12-10 08:53:23 +0100

    Correcting connection to stylesheet

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const connectDB = require('./config/db')
-const dotenv = require('dotenv').config()
+require('dotenv').config()
+const path = require('path')
 
 const app = express()
 const port = 3000
 
 connectDB();
+app.use(express.json())
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(__dirname + '/views/home.html')
+})
+app.get('/list', (req, res) => {
+  res.sendFile(__dirname + '/views/list.html')
+})
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/views/login.html')
+})
+app.get('/register', (req, res) => {
+  res.sendFile(__dirname + '/views/register.html')
 })
 
 app.listen(port, () => {
